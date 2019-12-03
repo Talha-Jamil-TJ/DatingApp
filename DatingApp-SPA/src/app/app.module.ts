cfa82a257057c928b61b8e6d28ed5d56772b8711
TJ <sufi.tj114@gmail.com>
2019-12-03 21:17:13 +0500

    Added User interface, user service, members list, detail components, jwt interceptor, etc

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,18 +3,28 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {RouterModule} from '@angular/router';
-import {BsDropdownModule} from 'ngx-bootstrap';
+import {BsDropdownModule, TabsModule} from 'ngx-bootstrap';
+import {JwtModule} from '@auth0/angular-jwt';
 
 import {AppComponent} from './app.component';
 import {NavComponent} from './nav/nav.component';
 import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
 import {ErrorInterceptorProvider} from './_services/error.interceptor';
-import {MemberListComponent} from './member-list/member-list.component';
+import {MemberListComponent} from './members/member-list/member-list.component';
 import {ListsComponent} from './lists/lists.component';
 import {MessagesComponent} from './messages/messages.component';
 import {appRoutes} from './routes';
 import {AuthGuard} from './_guards/auth.guard';
+import {MemberCardComponent} from './members/member-card/member-card.component';
+import {MemberDetailComponent} from './members/member-detail/member-detail.component';
+import {MemberDetailResolver} from './_resolvers/member-detail.resolver';
+import {MemberListsResolver} from './_resolvers/member-lists.resolver';
+import {NgxGalleryModule} from 'ngx-gallery';
+
+export function localTokenGetter() {
+  return localStorage.getItem('token');
+}
 
 @NgModule({
   declarations: [
@@ -25,16 +35,32 @@ import {AuthGuard} from './_guards/auth.guard';
     MemberListComponent,
     ListsComponent,
     MessagesComponent,
+    MemberCardComponent,
+    MemberDetailComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BsDropdownModule.forRoot(),
+    TabsModule.forRoot(),
+    NgxGalleryModule,
     RouterModule.forRoot(appRoutes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: localTokenGetter,
+        whitelistedDomains: ['localhost:5001'],
+        blacklistedRoutes: ['localhost:5001/api/auth'],
+      },
+    }),
   ],
 
-  providers: [ErrorInterceptorProvider, AuthGuard],
+  providers: [
+    ErrorInterceptorProvider,
+    AuthGuard,
+    MemberDetailResolver,
+    MemberListsResolver,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {
